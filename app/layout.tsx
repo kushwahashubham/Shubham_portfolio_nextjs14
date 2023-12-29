0eb05e71513c526dc790b1a7ab2887efc171bb49
Shubham Kushwaha <shubham.kushwaha.star@gmail.com>
2023-12-29 16:32:22 +0530

    tailwind config is done, also added some utilities classes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
+/* eslint-disable camelcase */
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import React from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const base = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-base",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={base.variable}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "primary-color": "#edf2f8",
+        "secondary-color": "#313bac",
+        "black-color": "#030303",
+        "lightGray-color": "#e4e4e4",
+        "gray-color": "#6b7688",
+        "brown-color": "#46364a",
+        "white-color": "#ffffff",
+      },
+      fontFamily: {
+        base: ["var(--font-base)"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
