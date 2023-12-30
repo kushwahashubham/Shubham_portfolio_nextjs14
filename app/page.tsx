1c68e16014d63e7038a2a51bc66c2655b4b7292e
Shubham Kushwaha <shubham.kushwaha.star@gmail.com>
2023-12-30 19:15:54 +0530

    initial file and folder strcture is done, also Sanity schemas are done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import { About, Footer, Testimonial, Header, Skills, Work } from "../container";
+import { Navbar } from "../components";
+
 export default function Home() {
   return (
-    <div className="* flex-center">
-      <h1 className=" font-bold text-2xl font-base text-secondary-color">
-        Hello! Welcome to nextjs 14.
-      </h1>
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   );
 }
